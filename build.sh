c767685807b04258316bb52fa7e0452e6deebd05
JuanLv <5554102+olbb@users.noreply.github.com>
2024-05-01 22:34:12 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,6 +13,12 @@ export OP_BUILD_PATH=$PWD
 git clone https://github.com/coolsnowwolf/lede.git
 
 cd "${OP_BUILD_PATH}"/lede || exit
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;luci-smartdns-dev' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
 ./scripts/feeds update -a && ./scripts/feeds install -a
 rm -rf ./tmp && rm -rf .config
 mv "${OP_BUILD_PATH}"/.config "${OP_BUILD_PATH}"/lede/.config
