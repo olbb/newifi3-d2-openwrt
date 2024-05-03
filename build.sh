f5c6b67f769fe8781ce5c584d53310a4d82b8b48
JuanLv <5554102+olbb@users.noreply.github.com>
2024-05-03 11:44:00 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -35,6 +35,7 @@ sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_genera
 make defconfig
 make download -j8
 # make V=s -j$(nproc)
+make image
 make package/ddns-go/luci-app-ddns-go/compile V=s
 
 echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>"$GITHUB_ENV"
