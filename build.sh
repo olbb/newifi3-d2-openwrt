62cdf797de1ef6ffe9c5120e25cfb1643768ece3
JuanLv <5554102+olbb@users.noreply.github.com>
2025-10-01 12:43:06 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,7 +37,7 @@ mv "${OP_BUILD_PATH}"/.config "${OP_BUILD_PATH}"/lede/.config
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 make defconfig
 make download -j8
-make V=s -j$(nproc)
+make -j$(nproc) || make -j1 || make -j1 V=s
 
 echo "FILE_DATE=$(date +%Y%m%d%H%M)" >>"$GITHUB_ENV"
 echo "Build finished..."
